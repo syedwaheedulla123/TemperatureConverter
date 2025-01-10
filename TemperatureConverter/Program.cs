947a12651c5aad0d1090a887dc32ba6137ffaeff
syed.waheedulla <syed.waheedulla@DESK002>
2025-01-10 15:03:54 +0530

    Add project files.

---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConverter
+{
+    internal class Program
+    { public double CelsiusToFahrenheit(Double Temperature) {
+            return (Temperature*9/5)+32;
+        }
+        public double FahrenheitToCelsius(Double Temperature)
+        {
+            return (Temperature - 32)*5/9;
+        }
+
+
+        static void Main(string[] args)
+        {
+            double Temperature, result;
+            Program Obj= new Program();
+            Console.WriteLine("Enter temperature in Celsius:");
+            Temperature = Convert.ToDouble(Console.ReadLine());
+            result = Obj.CelsiusToFahrenheit(Temperature);
+            Console.WriteLine($"{Temperature} Celsius = {result} Fahrenheit");
+
+            Console.WriteLine("Enter temperature in Fahrenheit:");
+            Temperature = Convert.ToDouble(Console.ReadLine());
+            result = Obj.FahrenheitToCelsius(Temperature);
+            Console.WriteLine($"{Temperature} Fahrenheit = {result} Celsius");
+        }
+    }
+}
